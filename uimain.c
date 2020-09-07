227035c497fcac2469dfae266e8119e164cac415
JAFavela <jafavela@miners.utep.edu>
2020-09-07 10:34:33 -0600

    Added support and implementation of the 5x7-font

--- uimain.c
+++ uimain.c
@@ -26,8 +26,9 @@ int main()
       break;
     case 'c':
       puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+      for (char c = 'A'; c < 'G'; c++)
+	//print_char_11x16(c);
+	print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
