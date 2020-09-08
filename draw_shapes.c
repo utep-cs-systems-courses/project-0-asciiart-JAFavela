cca7acca8899fa9d3974a2e3f20798a1147a885d
JAFavela <jafavela@miners.utep.edu>
2020-09-07 18:40:35 -0600

    Added arrow implementation to draw_shapes and option in uimain

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,23 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+// Prints an arrow
+void print_arrow(int leftCol, int size)
+{
+   for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+   }
+  int i, j;
+  int startCol = leftCol  + (size / 4);
+  int endCol = leftCol + size * 2;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col <= startCol; col++) putchar(' ');
+    for (       ; col <=  endCol-2; col++) putchar('*');
+    putchar('\n');
+  }
+}
